692a655368413a380f62456bbc554a6008682792
Admin <Admin@DESKTOP-D6KRFC3>
2022-06-08 15:26:21 +0530

    First Commit

---
+++ C2TC/src/dateAndTime/demo.java
@@ -0,0 +1,21 @@
+package dateAndTime;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		LocalDate currDate = LocalDate.now();
+		DayOfWeek dayOfweek = currDate.getDayOfWeek(); // getDayOfWeek(); returns a DayOfWeek instance
+		
+		int dayOfMonth = currDate.getDayOfMonth();
+		int dayOfYear = currDate.getDayOfYear();
+
+		System.out.println("Date: " + currDate);
+		System.out.println("Day of Week: " + dayOfweek);
+		System.out.println("Day of Month: " + dayOfMonth);
+		System.out.println("Day of Year: " + dayOfYear);
+	}
+}
